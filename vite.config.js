e10761c0494523bdd3812d54cfefbfd6db0fbcdf
mmssajith <mmssajith@gmail.com>
2026-02-28 11:36:59 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/mini-online-games/',
   build: {
     rollupOptions: {
       input: {
